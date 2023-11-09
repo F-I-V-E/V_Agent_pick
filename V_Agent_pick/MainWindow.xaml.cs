ab345ba66984eb5d274f60f807005a308d2476c6
Five <59027740+F-I-V-E@users.noreply.github.com>
2023-11-09 18:08:14 +0100

    unneeded Using directives removed

--- V_Agent_pick/MainWindow.xaml.cs
+++ V_Agent_pick/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Resources;
-using System.Reflection;
 
 namespace V_Agent_pick
 {
@@ -120,9 +108,8 @@ namespace V_Agent_pick
             {
                 if (c[i]) count++;
             }
-
             agents = new string[count];
-            int f = 0; //index for agents array
+            int f = 0;
             for (int i = 0; i < c.Length; i++)
             {
                 if (c[i])
